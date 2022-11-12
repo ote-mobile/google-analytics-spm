c53589076f90680d5914828db0c5f05da6fb08a9
Ruslan Samsonov <rsamsonov@devexperts.com>
2022-11-12 16:14:07 +0300

    binary files permissions

--- prepare.sh
+++ prepare.sh
@@ -16,4 +16,8 @@ tar -xzf GoogleAnalytics-${VERSION}.tar.gz --directory .
 
 cp -r  GoogleAnalytics-${VERSION}/Frameworks/GoogleAnalytics.xcframework GoogleAnalytics.xcframework
 
+chmod 755 GoogleAnalytics.xcframework/ios-arm64_armv7/GoogleAnalytics.framework/GoogleAnalytics 
+chmod 755 GoogleAnalytics.xcframework/ios-arm64_i386_x86_64-simulator/GoogleAnalytics.framework/GoogleAnalytics 
+chmod 755 GoogleAnalytics.xcframework/ios-arm64_x86_64-maccatalyst/GoogleAnalytics.framework/GoogleAnalytics 
+
 clean
